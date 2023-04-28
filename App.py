9fccebfb9aa05813a824c56bc965b9e974ed3b58
saiveen1 <saiveen.wang@gmail.com>
2023-04-29 05:09:26 +0800

    3

--- App.py
+++ App.py
@@ -1,26 +1,9 @@
-import PySimpleGUI as sg
+import gui
 
-data = [['1', '2', '3'],
-        ['4', '5', '6'],
-        ['7', '8', '9']]
 
-layout = [[sg.Table(values=data, headings=['Column 1', 'Column 2', 'Column 3'], num_rows=10,
-          key='-TABLE-', enable_events=True, auto_size_columns_to_fit=True)],
-          [sg.Button('Modify Selected Cell'), sg.Button('Exit')]]
+def main():
+    gui.show_window()
 
-window = sg.Window('Table Modification', layout)
 
-while True:
-    event, values = window.read()
-    if event == sg.WIN_CLOSED or event == 'Exit':
-        break
-    if event == 'Modify Selected Cell':
-        selected_rows = values['-TABLE-'][0]
-        selected_cols = values['-TABLE-'][1]
-        if selected_rows and selected_cols:
-            row, col = selected_rows[0], selected_cols[0]
-            data[row][col] = sg.popup_get_text('Enter new value', default_text=data[row][col])
-            window['-TABLE-'].update(values=data)
-
-window.close()
-SelectedRowIndexes，GetSelectedRow，get，SelectedRows,auto_size_columns_to_fit
+if __name__ == '__main__':
+    main()

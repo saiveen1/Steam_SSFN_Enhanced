18977bd6b2d64a8efa05dd917a8ab6ececeaa187
saiveen1 <saiveen.wang@gmail.com>
2023-05-18 02:06:43 +0800

    both version

--- steam_account.py
+++ steam_account.py
@@ -1,5 +1,4 @@
 import datetime
-import queue
 import re
 import threading
 
@@ -8,32 +7,60 @@ from steam.webapi import WebAPI
 
 
 class SteamAccount:
+    api = None
+    thread = None
+    initialized = False
+
+    @staticmethod
+    def get_webAPI():
+        if not SteamAccount.initialized:
+            thread = threading.Thread(target=SteamAccount.thread_webapi, args=())
+            thread.start()
+            thread.join()
+            SteamAccount.initialized = True
+
+    @staticmethod
+    def thread_webapi():
+        try:
+            SteamAccount.api = WebAPI(key='0F6C12E262EE5101755F668842217EE7')
+        # except requests.exceptions as exp:
+        except requests.exceptions.SSLError:
+            SteamAccount.api = 'Net Error'
 
     def __init__(self, account_str):
-        self.account_str = account_str
+        self.is_danger = None
+        self.str_acc = account_str
         self.steam_id = None
-        self.api = None
-        self.thread = None
-        self.thread = threading.Thread(target=self.thread_webapi, args=())
-        self.thread.start()
-
+        self.d_acc_info = None
+        self.get_account_info(api_check=False)
     # 会占用线程时间, 单启动一个线程获取
-    def thread_webapi(self):
-        self.api = WebAPI(key='0F6C12E262EE5101755F668842217EE7')
 
     def parse_login_info(self) -> dict[str:str]:
-        pattern = r'(\S+?)----(\S+?)----(ssfn\d+)(----(\d+))?'  # 匹配7位数字的正则表达式
-        result = re.findall(pattern, self.account_str)
-        if result:
-            self.steam_id = result[0][4] if result[0][4] else ''  # 判断是否存在7位数字
-            return {"username": result[0][0], "password": result[0][1], "ssfn": result[0][2], "steamid": self.steam_id}
-        else:
-            return None
+        if self.d_acc_info is None:
+            pattern = r'(\S+?)----(\S+?)----(ssfn\d+)----(\d+)?'  # 匹配17位数字的正则表达式
+            result = re.findall(pattern, self.str_acc)
+
+            # 可以加一个括号，匹配出来的会多一个----
+            if len(result) == 0:
+                pattern = r'(\S+?)----(\S+?)----(ssfn\d+)'
+                result = re.findall(pattern, self.str_acc)
+            if result:
+                self.steam_id = '' if len(result[0]) == 3 else result[0][3] if result[0][3] else ''  # 判断是否存在17位数字
+                remark, sale = self.str_acc.split('----'.join([i for i in result[0] if i]))
+                self.d_acc_info = {"remark": remark.rstrip(),
+                                   "username": result[0][0], "password": result[0][1],
+                                   "ssfn": result[0][2], "steamid": self.steam_id,
+                                   "sale_info": sale.lstrip('----')
+                                   }
+                return self.d_acc_info
+            else:
+                return None
 
     def get_games_ban(self) -> dict[str:str]:
-        if self.api is None:
+        if SteamAccount.api is None:
             self.thread.join()
-        data = self.api.ISteamUser.GetPlayerBans(format='json', steamids=self.steam_id)
+
+        data = SteamAccount.api.ISteamUser.GetPlayerBans(format='json', steamids=self.steam_id)
         # api判断的不准确，各种情况，还是要直接通过cookie 获取封禁具体
         days_since_last_ban = data['players'][0].pop('DaysSinceLastBan')
         last_ban_time = ''
@@ -41,38 +68,52 @@ class SteamAccount:
             last_ban_time = datetime.datetime.now() - datetime.timedelta(days=days_since_last_ban)
             last_ban_time = last_ban_time.replace(second=0, microsecond=0).strftime('%Y-%m-%d')
         data['players'][0]['LastBanTime'] = last_ban_time
-        # <class 'dict'> {'players':
-        # [{'SteamId': '76561198425601856', 'CommunityBanned': False,
-        # 'VACBanned': False, 'NumberOfVACBans': 0, 'NumberOfGameBans': 1,
-        # 'EconomyBan': 'none', 'LastBanTime': '2023-04-13'}]}
-        data = data['players'][0]
+        return data['players'][0]
 
-        # print(type(data), data)
-        # table_header = ['username', 'password', 'ssfn', 'steamid', 'GameBans', 'LastBan', 'OwnCsGo', 'VacBan']
-        last_ban_time = data.pop('LastBanTime')
-        data['LastBan'] = last_ban_time
-        game_bans = data.pop('NumberOfGameBans')
-        data['GameBans'] = game_bans
-        vac_ban = data.pop('NumberOfVACBans')
-        data['VacBan'] = vac_ban
-
-        return data
+    def get_game_info(self) -> dict[str:list]:
+        data = SteamAccount.api.IPlayerService.GetOwnedGames(format='json', steamid=self.steam_id,
+                                                             include_appinfo=True,
+                                                             include_played_free_games=False,
+                                                             appids_filter=0,
+                                                             include_free_sub=False,
+                                                             language='	it',
+                                                             include_extended_appinfo=True
+                                                             )
+        return data['response']
 
     def get_account_info(self, api_check=False) -> dict[str:str]:
-        login_info = self.parse_login_info()
-        if login_info is None:
+        # 这里可以优化以下，不过懒得弄了
+        d_login_info = self.parse_login_info()
+        if d_login_info is None:
             return None
-        if login_info['steamid'] == '' or api_check is False:
-            return login_info
+        if d_login_info['steamid'] == '' or api_check is False:
+            return d_login_info
         bans_info = None
+        game_info = None
         try:
+            if SteamAccount.api is None:
+                SteamAccount.get_webAPI()
+            if SteamAccount.api == 'Net Error':
+                return -3, SteamAccount.api
             bans_info = self.get_games_ban()
+            game_info = self.get_game_info()
+        except Exception as exp:
+            return -1, exp
         except requests.exceptions.SSLError:
             bans_info['GameBans'] = 'Net error'
         except requests.exceptions.RequestException as exp:
-            return -1, exp
+            return -2, exp
 
-        return {**login_info, **bans_info}
+        if bans_info['NumberOfGameBans'] > 0:
+            self.is_danger = True
+            if d_login_info['remark'] != '':
+                self.str_acc = self.str_acc.replace(d_login_info['remark'], '永久', 1)
+            else:
+                self.str_acc = '永久 ' + self.str_acc
+            d_login_info['remark'] = '永久'
+
+        self.d_acc_info = {**d_login_info, **bans_info, **game_info}
+        return self.d_acc_info
 
     def get_owned_games(self):
         # ...
--- steam_login.py
+++ steam_login.py
@@ -7,19 +7,14 @@ import time
 import requests
 import winreg
 
-loc_path = './ssfn_local'
-headers = {
-    'user-agent': 'Mozilla/4.0 (compatible; MSIE 9.0; Windows NT 6.1)',
-    'Content-Type': 'application/x-www-form-urlencoded'
-}
-url = r'https://ssfnbox.com/download/'
-backup_url = r'http://tool.ctrl000.cc:66/ssfn/'
+import values
 
 
 class SteamLogin:
     def __init__(self, steam_path=None):
         self.steam_path = None
         self.ssfn_str = None
+        self.current_path = None
 
     @staticmethod
     def get_steam_path():
@@ -29,86 +24,124 @@ class SteamLogin:
 
     def init_ssfn(self):
         # 创建本地ssfn库
-        if not os.path.exists(loc_path):
-            os.makedirs(loc_path)
+        if not os.path.exists(values.loc_path):
+            os.makedirs(values.loc_path)
 
         # 删除steam目录下的ssfn文件
         for filename in os.listdir(self.steam_path):
             if re.match(r'ssfn.*', filename):
                 os.remove(os.path.join(self.steam_path, filename))
 
-    def save_file(self, ssfn_str, ssfn_response):
+    def save_ssfn_file(self, ssfn_str, ssfn_response):
         filename = self.steam_path + '/' + ssfn_str
         with open(filename, 'wb') as f:
             f.write(ssfn_response.content)
 
         # 保存到本地
-        filename = loc_path + '/' + ssfn_str
+        filename = values.loc_path + '/' + ssfn_str
         with open(filename, 'wb') as f:
             f.write(ssfn_response.content)
             # print(f'已成功写入文件 {filename}')
 
-    def ssfn_download(self, ssfn_str):
-        self.steam_path = self.get_steam_path()
+    def ssfn_download(self, ssfn_str, steam_path):
+        # 因为steam路径可能会改变，不能用成员变量
+        self.steam_path = steam_path
+
         self.ssfn_str = ssfn_str
-        send_url = url + self.ssfn_str
+        send_url = values.ssfnbox_url + self.ssfn_str
         self.init_ssfn()
 
         # 判断本地库是否已经存在
-        filename = loc_path + '/' + self.ssfn_str
-        if os.path.exists(filename):
-            shutil.copy(filename, self.steam_path)
+        filename = values.loc_path + '/' + self.ssfn_str
+        ssfn_response = None
+        l_status = []
+        try:
+            if os.path.exists(filename):
+                shutil.copy(filename, self.steam_path)
+        except PermissionError:
+            return values.ERRORS.WRITE_PERMISSION_ERROR
         # 如果不存在从网上下载
         else:
             try:
-                result = requests.get(send_url, headers=headers, verify=False)
+                result = requests.get(send_url, headers=values.headers, verify=False)
                 # 防止爬虫延迟获得
                 # 如果没找到抛出AttributeError异常
                 code = re.search(r'\?sec=\w+', result.content.decode('utf-8')).group(0)
                 result.close()
                 send_url = send_url + code
-                ssfn_response = requests.get(send_url, headers=headers, verify=False)
-
+                ssfn_response = requests.get(send_url, headers=values.headers, verify=False)
+                self.save_ssfn_file(ssfn_str, ssfn_response)
             # ssfnbox未找到授权使用另一个地址
             # 一键授权逆向地址
             except AttributeError:
-                try:
-                    send_url = backup_url + self.ssfn_str
-                    ssfn_response = requests.get(send_url, headers=headers, verify=False)
+                for url in values.l_backup_urls:
+                    send_url = url + self.ssfn_str
                     try:
-                        # 未找到授权
-                        is_404 = True if ssfn_response.content.decode('utf-8').find('Not Found') > 0 else False
-                        if is_404 is True:
-                            return -1
-                        ssfn_response.close()
-                    # 此异常代表下载到了正确文件，非“异常”，只是写法上比较简单
-                    except UnicodeDecodeError:
-                        pass
-                except Exception as exp:
-                    return exp
+                        ssfn_response = requests.get(send_url, headers=values.headers, verify=False)
+                        # 连接超时
+                        if ssfn_response.status_code == 502:
+                            l_status.append(values.ERRORS.SSFNError.SSFN_DOWNLOAD_ERROR)
+                            continue
+                        try:
+                            # 未找到授权
+                            is_404 = True if ssfn_response.content.decode('utf-8').find('Not Found') > 0 else False
+                            if is_404 is True:
+                                l_status.append(values.ERRORS.STEAM_NOT_FOUND_ERROR)
+                                continue
+                        # 此异常代表下载到了正确文件，非“异常”，只是写法上比较简单
+                        except UnicodeDecodeError:
+                            self.save_ssfn_file(ssfn_str, ssfn_response)
+                            break
+                    except requests.exceptions.SSLError:
+                        l_status.append(values.ERRORS.SSFNError.NET_SSL_ERROR)
+                        continue
+                    except Exception as exp:
+                        return exp
 
-            self.save_file(ssfn_str, ssfn_response)
-            return None
+            ssfn_response.close()
+            # 只返回最后一个备用网址的状态
+            return None if len(l_status) == 0 else l_status[len(l_status) - 1]
 
-    def login(self, is_old, account):
+    # 虽然self 里有参数path，但是前端path可能会改变，所以还是需要传递一个参数
+    def login(self, d_acc_info, steam_path):
+        self.steam_path = steam_path
+
+        # # 检查sys.check 是否为True以确定是否经过了pyinstaller 打包
+        # if getattr(sys, 'frozen', False):
+        #     self.current_path = os.path.abspath(sys.executable)
+        # else:
+        #     self.current_path = os.getcwd()
+        self.current_path = os.getcwd()
+
         subprocess.Popen('taskkill /F /IM steam.exe', creationflags=subprocess.DETACHED_PROCESS)
-        # print("正在启动或者关闭steam")
         time.sleep(1)
-        ssfn_ret = self.ssfn_download(account['ssfn'])
+        ssfn_ret = self.ssfn_download(d_acc_info['ssfn'], self.steam_path)
         # 下载出错
-        if ssfn_ret == -1:
-            return '未找到授权！请自行将授权放到ssfn_local登录'
-        # 下载异常
+
+        if ssfn_ret == values.ERRORS.WRITE_PERMISSION_ERROR:
+            return 'ssfn写入失败，请以管理员启动！'
+        elif ssfn_ret in vars(values.ERRORS.SSFNError).values() and ssfn_ret is not None:
+            p = os.path.normpath(os.path.join(self.current_path, values.loc_path))
+            subprocess.Popen(f'explorer {p}')
+            t = '' if ssfn_ret == values.ERRORS.SSFNError.SSFN_NOT_FOUND else '备用网站连接超时，'
+            return t + '未找到授权！\n请自行将授权放到ssfn_local登录'
+        # 其他异常
         elif ssfn_ret is not None:
             return ssfn_ret
 
         time.sleep(1)
-        steam = self.steam_path + '/' + r"steam.exe"
-        if is_old is True:
-            # 老版steam
-            subprocess.Popen(steam + ' -login ' + str(account['username']) + ' ' + str(account['password']))
-        else:
-            subprocess.Popen(
-                steam + ' -windowed -bigpicture -login ' + str(account['username']) + ' ' +
-                str(account['password']))
-        return None
+
+        is_old = True if os.path.exists(self.steam_path + '/' + r"steam.cfg") else False
+
+        steam_exe = self.steam_path + '/' + r"steam.exe"
+        try:
+            if is_old is True:
+                # 老版steam
+                subprocess.Popen(steam_exe + ' -login ' + str(d_acc_info['username']) + ' ' + str(d_acc_info['password']))
+            else:
+                subprocess.Popen(
+                    steam_exe + ' -windowed -bigpicture -login ' + str(d_acc_info['username']) + ' ' +
+                    str(d_acc_info['password']))
+        except FileNotFoundError:
+            subprocess.Popen(f'explorer {os.path.normpath(self.steam_path)}')
+            return 'steam.exe未找到，请确认路径或命名是否正确！'
